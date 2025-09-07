b2b5b52e9843c82e7327cdf7ac0dac009dea1546
Ilnar Khusainov <ilnarrkhusainov@yandex.ru>
2025-09-07 20:50:05 +0500

    add-controllers

--- src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
@@ -1,36 +1,65 @@
 package ru.practicum.shareit.exceptions;
 
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Map<String, String> handleNotFound(NotFoundException exception) {
+        log.error("NotFoundException: {}", exception.getMessage());
         return Map.of("error", exception.getMessage());
     }
 
     @ExceptionHandler(ValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleValidation(ValidationException exception) {
+        log.error("ValidationException: {}", exception.getMessage());
         return Map.of("error", exception.getMessage());
     }
 
     @ExceptionHandler(ForbiddenException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public Map<String, String> handleForbidden(ForbiddenException exception) {
+        log.error("ForbiddenException: {}", exception.getMessage());
         return Map.of("error", exception.getMessage());
     }
 
     @ExceptionHandler(ConflictException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public Map<String, String> handleConflict(ConflictException exception) {
+        log.error("ConflictException: {}", exception.getMessage());
         return Map.of("error", exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
+        log.error("MethodArgumentNotValidException: {}", exception.getMessage());
+        return Map.of("error", "Некорректные данные: " + exception.getFieldError().getDefaultMessage());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleConstraintViolation(ConstraintViolationException exception) {
+        log.error("ConstraintViolationException: {}", exception.getMessage());
+        return Map.of("error", "Ошибка валидации: " + exception.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleAll(Exception exception) {
+        log.error("Unexpected error: ", exception);
+        return Map.of("error", "Внутренняя ошибка сервера");
     }
 }
