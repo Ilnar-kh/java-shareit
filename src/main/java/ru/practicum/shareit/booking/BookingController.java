f9a99e15929c723c18c72948e8ad2cbd8ed44476
Ilnar Khusainov <ilnarrkhusainov@yandex.ru>
2025-09-21 20:21:19 +0500

    add-bookings

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,6 +1,9 @@
 package ru.practicum.shareit.booking;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingResponseDto;
@@ -8,6 +11,7 @@ import ru.practicum.shareit.booking.dto.BookingState;
 
 import java.util.List;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/bookings")
@@ -16,32 +20,32 @@ public class BookingController {
     private final BookingService service;
 
     @PostMapping
-    public BookingResponseDto create(@RequestHeader("X-Sharer-User-Id") Long userId,
-                                     @RequestBody BookingDto dto) {
+    public BookingResponseDto create(@RequestHeader("X-Sharer-User-Id") @Positive Long userId,
+                                     @RequestBody @Valid BookingDto dto) {
         return service.create(userId, dto);
     }
 
     @PatchMapping("/{bookingId}")
-    public BookingResponseDto approve(@RequestHeader("X-Sharer-User-Id") Long userId,
-                                      @PathVariable Long bookingId,
+    public BookingResponseDto approve(@RequestHeader("X-Sharer-User-Id") @Positive Long userId,
+                                      @PathVariable @Positive Long bookingId,
                                       @RequestParam boolean approved) {
         return service.approve(userId, bookingId, approved);
     }
 
     @GetMapping("/{bookingId}")
-    public BookingResponseDto getById(@RequestHeader("X-Sharer-User-Id") Long userId,
-                                      @PathVariable Long bookingId) {
+    public BookingResponseDto getById(@RequestHeader("X-Sharer-User-Id") @Positive Long userId,
+                                      @PathVariable @Positive Long bookingId) {
         return service.getById(userId, bookingId);
     }
 
     @GetMapping
-    public List<BookingResponseDto> getForBooker(@RequestHeader("X-Sharer-User-Id") Long userId,
+    public List<BookingResponseDto> getForBooker(@RequestHeader("X-Sharer-User-Id") @Positive Long userId,
                                                  @RequestParam(defaultValue = "ALL") BookingState state) {
         return service.getForBooker(userId, state);
     }
 
     @GetMapping("/owner")
-    public List<BookingResponseDto> getForOwner(@RequestHeader("X-Sharer-User-Id") Long userId,
+    public List<BookingResponseDto> getForOwner(@RequestHeader("X-Sharer-User-Id") @Positive Long userId,
                                                 @RequestParam(defaultValue = "ALL") BookingState state) {
         return service.getForOwner(userId, state);
     }
--- src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
@@ -1,11 +1,14 @@
 package ru.practicum.shareit.booking.mapper;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.Booking;
 import ru.practicum.shareit.booking.dto.BookingResponseDto;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.User;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class BookingMapper {
     public static Booking toEntity(BookingDto dto, User booker, Item item) {
         return Booking.builder()
